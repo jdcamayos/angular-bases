39b831e6fc8963879b588162af9c1f594d1b0b75
Juan Camayo <jd.camayo@gmail.com>
2023-08-17 11:27:16 -0500

    Add - some changes and knowing ngIf in hero component

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  public name: string = 'ironman'
+  public age: number = 45
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase()
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero(): void {
+    this.name = 'spiderman'
+  }
+
+  changeAge(): void {
+    this.age = 25
+  }
+
+  resetForm() {
+    this.name = 'ironman'
+    this.age = 45
+  }
+
 }
